913b9a37ea8397c2bede90c3ab4dfb115e25ab2b
Fantomen <benjaminhagglund@hotmail.com>
2023-12-03 23:35:33 +0200

    Forward is always where the camera is looking

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
+@onready var twist_pivot := $TwistPivot
+@onready var pitch_pivot := $TwistPivot/PitchPivot
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -15,14 +18,14 @@ func _process(delta):
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
 	
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	$TwistPivot.rotate_y(twist_input)
-	$TwistPivot/PitchPivot.rotate_x(pitch_input)
-	$TwistPivot/PitchPivot.rotation.x = clamp($TwistPivot/PitchPivot.rotation.x,-0.5,0.5)
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x,-0.8,0.8)
 	
 	twist_input = 0.0
 	pitch_input = 0.0
